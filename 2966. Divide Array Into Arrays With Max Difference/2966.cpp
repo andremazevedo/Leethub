84ea482bd8c43c344c0b927c863eed49b9308702
andremazevedo <andremazevedo6@gmail.com>
2024-02-01 19:14:30 -0100

    2966

---
+++ 2966. Divide Array Into Arrays With Max Difference/2966.cpp
@@ -0,0 +1,33 @@
+#include "../streamUtils.hpp"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        vector<vector<int>> answer;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i += 3) {
+            if (nums[i + 2] - nums[i] > k)
+                return {};
+            
+            answer.push_back({nums[i], nums[i + 1], nums[i + 2]});
+        }
+        
+        return answer;
+    }
+};
+
+int main() {
+
+    vector<int> nums = {1,3,4,8,7,9,3,5,1};
+    int k = 4;
+    
+    cout << "Input: nums = " << nums << ", n = " << k << endl;
+    
+    cout << "Output: "<< Solution().divideArray(nums, k) << endl;
+
+    return 0;
+}
