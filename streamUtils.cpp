acb4e7f4874db817407d20ddaf8004d6b7825520
andremazevedo <andremazevedo6@gmail.com>
2024-04-08 19:05:49 +0100

    IO changes

--- streamUtils.cpp
+++ streamUtils.cpp
@@ -1,83 +1,5 @@
 #include "streamUtils.hpp"
 
-std::ostream& operator<<(std::ostream& os, const std::vector<int>& vec)
-{
-    os << "[";
-    for (size_t i = 0; i < vec.size(); ++i) {
-        os << vec[i];
-        if (i < vec.size() - 1) {
-            os << ",";
-        }
-    }
-    os << "]";
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const std::vector<double>& vec)
-{
-    os << "[";
-    for (size_t i = 0; i < vec.size(); ++i) {
-        os << vec[i];
-        if (i < vec.size() - 1) {
-            os << ",";
-        }
-    }
-    os << "]";
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<int>>& matrix)
-{
-    os << "[";
-    for (size_t row = 0; row < matrix.size(); ++row) {
-        os << matrix[row];
-        if (row < matrix.size() - 1) {
-            os << ",";
-        }
-    }
-    os << "]";
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<double>>& matrix)
-{
-    os << "[";
-    for (size_t row = 0; row < matrix.size(); ++row) {
-        os << matrix[row];
-        if (row < matrix.size() - 1) {
-            os << ",";
-        }
-    }
-    os << "]";
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const std::vector<std::string>& vec)
-{
-    os << "[";
-    for (size_t i = 0; i < vec.size(); ++i) {
-        os << "\"" << vec[i] << "\"";
-        if (i < vec.size() - 1) {
-            os << ",";
-        }
-    }
-    os << "]";
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const std::vector<TreeNode*>& vec)
-{
-     os << "[";
-    for (size_t i = 0; i < vec.size(); ++i) {
-        os << vec[i]->val;
-        if (i < vec.size() - 1) {
-            os << ",";
-        }
-    }
-    os << "]";
-    return os;
-}
-
 std::ostream& operator<<(std::ostream& os, const ListNode* head)
 {
     os << "[";
@@ -175,3 +97,13 @@ TreeNode* makeTree(const std::vector<int>& vec)
 
     return root;
 }
+
+// void input()
+// {
+//     std::cout << "Input: " << std::endl;
+// }
+
+// void output()
+// {
+//     std::cout << "Output: ";
+// }
--- streamUtils.hpp
+++ streamUtils.hpp
@@ -8,6 +8,9 @@
 #include <algorithm>
 #include <unordered_set>
 #include <unordered_map>
+#include <cctype>
+
+#define getName(VariableName) # VariableName
 
 struct ListNode {
     int val;
@@ -26,18 +29,6 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-std::ostream& operator<<(std::ostream& os, const std::vector<int>& vec);
-
-std::ostream& operator<<(std::ostream& os, const std::vector<double>& vec);
-
-std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<int>>& matrix);
-
-std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<double>>& matrix);
-
-std::ostream& operator<<(std::ostream& os, const std::vector<std::string>& vec);
-
-std::ostream& operator<<(std::ostream& os, const std::vector<TreeNode*>& vec);
-
 std::ostream& operator<<(std::ostream& os, const ListNode* head);
 
 ListNode* makeList(const std::vector<int>& vec);
@@ -45,5 +36,130 @@ ListNode* makeList(const std::vector<int>& vec);
 std::ostream& operator<<(std::ostream& os, const TreeNode* root);
 
 TreeNode* makeTree(const std::vector<int>& vec);
+
+template<typename T>
+void printArg(const T& arg) {
+    if (std::is_same_v<T, std::string>) {
+        std::cout << "\"" << arg << "\"";
+    } else if (std::is_same_v<T, char>) {
+        std::cout << "\"" << arg << "\"";
+    } else {
+        std::cout << arg;
+    }
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    os << "[";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        printArg(vec[i]);
+        if (i < vec.size() - 1) {
+            os << ",";
+        }
+    }
+    os << "]";
+    return os;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& matrix)
+{
+    os << "[";
+    for (size_t row = 0; row < matrix.size(); ++row) {
+        printArg(matrix[row]);
+        if (row < matrix.size() - 1) {
+            os << ",";
+        }
+    }
+    os << "]";
+    return os;
+}
+
+template<typename T>
+void printVal(const T& arg);
+
+// template<>
+// void printVal<std::string>(const std::string& str);
+
+template<typename T, typename... Types>
+void printVal(const T& arg, const Types&... args);
+
+// template<typename... Types>
+// void printVal(const std::string& str, const Types&... args);
+
+template<typename T>
+void printName(const T& arg);
+
+template<typename T, typename... Types>
+void printName(const T& arg, const Types&... args);
+
+// void printInput();
+
+template<typename T, typename... Types>
+void printInput(const T& arg, const Types&... args);
+
+// void printOutput();
+
+template<typename T> void printOutput(T arg);
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& vec);
+
+template<typename T>
+void printVal(const T& arg)
+{
+    printArg(arg);
+    std::cout << std::endl;
+}
+
+// template<>
+// void printVal<std::string>(const std::string& str)
+// {
+//     std::cout << "\"" << str << "\"" << std::endl;
+// }
+
+template<typename T, typename... Types>
+void printVal(const T& arg, const Types&... args)
+{
+    printArg(arg);
+    std::cout << ", ";
+    printName(args...);
+}
+
+// template<typename... Types>
+// void printVal(const std::string& str, const Types&... args)
+// {
+//     std::cout << "\"" << str << "\", ";
+//     printName(args...);
+// }
+
+template<typename T>
+void printName(const T& arg)
+{
+    std::cout << arg << " = " << std::endl;
+}
+
+template<typename T, typename... Types>
+void printName(const T& arg, const Types&... args)
+{
+    std::cout << arg << " = ";
+    printVal(args...);
+}
+
+template<typename T, typename... Types>
+void printInput(const T& arg, const Types&... args)
+{
+    std::cout << "Input: ";
+    printName(arg, args...);
+}
+
+template<typename T> void printOutput(T arg) 
+{
+    std::cout << std::boolalpha << "Output: ";
+    printArg(arg);
+    std::cout << std::endl;
+}
+
 
 #endif
